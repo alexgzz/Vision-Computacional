c748b7abb1e9855634c06603858614e194fccca7
alexgzz <alez_gzz@live.com>
2013-05-23 10:07:27 -0500

    Update proyecto.py
    eliminación de líneas en blanco agregadas por gedit(editor de texto)

--- proyecto.py
+++ proyecto.py
@@ -1,101 +1,54 @@
 import cv
 import Image, ImageDraw
-
 from sys import argv
 import time
 
 
-
 def filtrom(image):
-
   pixeles = image.load()
-
 	d = image.size
-
 	for i in range(d[0]):
-
 		for j in range(d[1]):
-
 			puntos = []
-
 			for x in range(-1,2):
-
 				for y in range(-1,2):
-
 					try:
-
 						puntos.append(pixeles[i+x,j+y][0])
-
 					except:
-
 						puntos.append(0)
 
-
-
 			puntos.sort()
-
 			h = len(puntos)/2
-
 			if len(puntos) % 2 == 0:
-
 				m = (puntos[h - 1])
-
 			else:
-
 				m = puntos[h]
-
-	
 
 			pixeles[i,j] = (m,m,m)
 
-			
-
 	#img2 = "filtrom_"+img
-
 	#image.save(img2)
-
 	return image
 
-
-
 def diferencia(image, image2):#filtrom, gris
-
 	pixeles1 = image.load()
-
 	pixeles2 = image2.load()
-
 	d = image.size
-
-		
-
 	esquinas = []
 
 	for i in range(d[0]):
-
 		for j in range(d[1]):
-
 			dif = (pixeles1[i,j][0]) - (pixeles2[i,j][0])
-
-							
-
+	
 			if dif >20:	
 
-				
-
 				pixeles2[i,j] = (255, 255, 255)
-
 				esquinas.append((i,j))
 
-				
-
 			else:
-
 				pixeles2[i,j] = (0,0,0)
 
-
-
 	#image2.save("esq_"+img)	
-
 	return esquinas
 
 def grises(image):
@@ -104,21 +57,15 @@ def grises(image):
 	d = image.size
 	#print "dimensiones: ", d
 	for i in range (0, d[0]):
-
 		for j in range (0, d[1]):
-
- 
-
 			pp = sum(pixeles[i,j])/3
-
 			pixeles[i,j] = (pp, pp, pp)
-						
+					
 	return image
 
 def bgr_to_rgb(image):
 	pixeles = image.load()
 	d = image.size
-
 	for i in range(d[0]):
 		for j in range(d[1]):
 			(b, g, r) = pixeles[i, j]
@@ -129,7 +76,6 @@ def bgr_to_rgb(image):
 def rgb_to_bgr(image):
 	pixeles = image.load()
 	d = image.size
-
 	for i in range(d[0]):
 		for j in range(d[1]):
 			(r, g, b) = pixeles[i, j]
@@ -170,108 +116,62 @@ def color_test(image):
 	x2 = x+420
 	y2 = y+420
 
-
-
 	rojos = []
-
 	verdes = []
-
 	azules = []
 
-	
-
 	for i in range(x, x2):
-
 		for j in range(y, y2):
-
 			(r, g, b)= pix[i, j]
-
 			rojos.append(r)
-
 			verdes.append(g)
-
 			azules.append(b)
 
-			
-
 	rp = (sum(rojos))/(len(rojos))
-
 	vp = (sum(verdes))/(len(verdes))
-
 	ap = (sum(azules))/(len(azules))
-
-
 
 	colorp = (rp, vp, ap)
 
 	#print "color pixel 1 =", pix[0,0]
-
 	#image = rgb_to_bgr(image)#rgb a bgr		
-
 
 	print "\nColor promedio =", colorp
 
-
 	#porcentajes de variacion
-
 	varr = ( ( (colorp[0]) - (colorref[0]) )/ 255.0 ) * 100.0 
-
 	varv = ( ( (colorp[1]) - (colorref[1]) )/ 255.0 ) * 100.0
-
 	vara = ( ( (colorp[2]) - (colorref[2]) )/ 255.0 ) * 100.0
-
-
 
 	var = (varr, varv, vara)
 
-
-
 	notas = []
-
 	for i in range(len(var)):
-
 		if(var[i] == 0):
-
 			notas.append("igual a la referencia y pasa la prueba")
-
 		elif (var[i] < 0):
-
 			if(abs(var[i]) < tolerancia):
-
 				notas.append("por debajo de la referencia y pasa la prueba")
-
 			else:
-
 				notas.append("por debajo de la referencia y no pasa la prueba")
-
 		else:
-
 			if(abs(var[i]) < tolerancia):
-
 				notas.append("por encima de la referencia y pasa la prueba")
-
 			else:
-
 				notas.append("por encima de la referencia y no pasa la prueba")
 
-
 	result = []
-
 	print "Variaciones del promedio respecto a la referencia:\n"
 	result.append("Variaciones del promedio respecto a la referencia:")
-	
 
 	print "Azul : ", "%.2f" % var[0], "%", notas[0]
 	result.append("Azul: " +str("%.2f" % var[0]) +"%" +" "+ str(notas[0]))
-	
 
 	print "Verde : ", "%.2f" % var[1], "%", notas[1]
 	result.append("Verde: " +str("%.2f" % var[1]) +"%" +" "+ str(notas[1]))
-	
 
 	print "Rojo : ", "%.2f" % var[2], "%", notas[2]
 	result.append("Rojo: " +str("%.2f" % var[2]) +"%" +" "+ str(notas[2]))
-	
 
 	return result	
 
@@ -300,7 +200,6 @@ def detectar_objeto(image): #checa los valores de los contornos dentro del recua
 		vs.append(sum((v1,v2,v3))/3)
 
 	detect = 0
-	#if(p1==p2 or p1==p3 or p1==p4 or p1==p5 or p2==p3 or p2==p4 or p2==p5 or p3==p4 or p3==p5 or p4==p5):
 	if(sum(vs)/len(vs) <= 10):
 		detect = 1
 	
@@ -335,7 +234,6 @@ while True:
 
     #esquinas = diferencia(gr_img, fm_img)
        
-    #info = ["Rojo 52% debajo de la referecia y no pasa","Verde 13% debajo de la referecia y no pasa","Azul 11% debajo de la referecia y no pasa"]
     p_img = pintar(p_img, 0)
 
     detect = detectar_objeto(p_img)
